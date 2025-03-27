4f1a3f5b5229b0b3e1867c4710c7a5eca47d8191
inseooo0 <calmandpassion@korea.ac.kr>
2025-03-27 16:55:35 +0900

    initial commit

---
+++ src/test/java/tobyspring/springboot_ac/SpringbootAcApplicationTests.java
@@ -0,0 +1,13 @@
+package tobyspring.springboot_ac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
